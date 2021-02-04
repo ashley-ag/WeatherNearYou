34e40fac1d0b4ab52bbb1dc886f9a821942e51e2
AG-gash <74515756+ashley-ag@users.nereply.github.com>
2021-02-03 19:50:03 -0800

    finished: current weather, and getting APIs to pull data; WIP: UV, 5 day & localStorage

--- javascript.js
+++ javascript.js
@@ -6,11 +6,21 @@ $(document).ready(function() {
   var humidityText = document.querySelector('#humidity');
   var windSpeedText = document.querySelector('#windSpeed');
   var UVIndexText = document.querySelector('#UVIndex');
+  var UVISpan = document.querySelector('#UVInumber');
+  
+
 
 
 $("#search-button").on("click", function() {
 //take the input from the search bar and fetch the data from open weather
-//console.log(searchInput.value);
+
+addEventListener("click", function(){
+  var name = nameInput.value;
+
+  var newLocalStorageEntry = {
+      citySearch: searchInput.value,
+  };
+
 //present todays weather data for specific city in the today card
 fetch('http://api.openweathermap.org/data/2.5/weather?q=' + searchInput.value + '&units=imperial&appid=01ba96e862d6d6a63299345f7c985a13')
     .then(response => response.json())
@@ -22,26 +32,41 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=' + searchInput.value + 
     var humidityVal = data['main']['humidity'];
     var windSpeedVal = data['wind']['speed'];
     var iconLink = "http://openweathermap.org/img/w/" + iconVal + ".png"
+    var long = data['coord']['lon'];
+    var lat = data['coord']['lat'];
 
     cityText.textContent = cityVal;
     $('#iconPNG').attr('src', iconLink);
     tempText.textContent = "Temperature: " + tempVal + "° F";
     humidityText.textContent = "Humidity: " + humidityVal + "%";
     windSpeedText.textContent = "Wind Speed: " + windSpeedVal + " MPH";
-    })
 
 //present 5 day forecast in forecast card
-    fetch('http://api.openweathermap.org/data/2.5/forecast/onecall?q=' + searchInput.value + '&cnt=4&units=imperial&appid=01ba96e862d6d6a63299345f7c985a13')
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&appid=01ba96e862d6d6a63299345f7c985a13')
     .then(response => response.json())
-    .then(data => console.log(data))
-    // .then(data => {
-    //   // the date, an icon representation of weather conditions, the temperature, and the humidity
-    //   var date = 
-    //   var icon5 = 
-    //   var temp5 =
-    //   var hum5 = 
-    // 
-  })
+   //.then(data => console.log(data))
+   .then(data => {
+     
+    
+    // the date, an icon representation of weather conditions, the temperature, and the humidity  
+      for(i=0; i > 4; i++) {
+
+      var date = new Date();
+      var icon5 = data['daily'][i]['weather'][i]['icon'];
+      var temp5 = data['daily'][i]['temp']['day'];
+      var hum5 = data['daily'][i]['humidity'];
+      var dateFore5 = document.getElementById("date" + [i]);
+      var forecastFore5 = document.getElementById("forecast" + [i]);    
+      
+      $(".card").removeClass("hidden");
+
+      dateFore5.textContent = date;
+      forecastFore5.textContent = icon5, temp5, hum5;
+
+      }
+
+    })
+})
 
 //store search history
 
@@ -53,4 +78,6 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=' + searchInput.value + 
 
 })
 
+})
 
+})
